c78d72b4597f907e98c828030afacd8b93b9d1ed
kamensky roman <kamensky.ra@gmail.com>
2021-10-27 23:27:31 +0300

    Initial Commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Роман Каменский on 27.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
