40c80e68e5d2c359c344fc734a41835e40dc60ee
kamensky roman <kamensky.ra@gmail.com>
2021-10-31 22:06:10 +0200

    login UI

--- Chat/ContentView.swift
+++ Chat/ContentView.swift
@@ -7,10 +7,70 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            ScrollView{
+                VStack(spacing: 18){
+                    Picker(selection: $isLoginMode, label: Text("Picker here")) {
+                        Text("Login")
+                            .tag(true)
+                        Text("Create account")
+                            .tag(false)
+                    }.pickerStyle(SegmentedPickerStyle())
+                    
+                    
+                    if !isLoginMode{
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "person.fill")
+                                .font(.system(size: 64))
+                                .padding()
+                        }
+                    }
+                    
+                    Group{
+                        TextField("Email", text: $email)
+                            .keyboardType(.emailAddress)
+                            .autocapitalization(.none)
+                        SecureField("Password", text: $password)
+                    }
+                    .padding(.vertical,12)
+                    .background(Color.white)
+                    
+                    
+                    
+                    Button {
+                        
+                    } label: {
+                        HStack{
+                            Spacer()
+                            Text(isLoginMode ? "Log In" : "Create account")
+                                .foregroundColor(.white)
+                                .padding(.vertical,10)
+                                .font(.system(size: 14, weight: .semibold))
+                            Spacer()
+                        }.background(.blue)
+                    }
+                }.padding()
+                
+            }
+            .navigationTitle(isLoginMode ? "Log In" : "Create account")
+            .background(Color(.init(white: 0, alpha: 0.05)))
+            
+        }
+    }
+    
+    private func handleAction(){
+        
     }
 }
 
